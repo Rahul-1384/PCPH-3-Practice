cebb466e54c2e72118b5c7e82ca61a66df167f94
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-04-11 20:06:56 +0530

    s

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        int sum=1405;
+        String str=Integer.toString(sum);
+        System.out.println(str);
+        
+    }    
+}
