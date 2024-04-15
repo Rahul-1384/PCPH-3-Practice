417bd92ba570d7f645d6e86999fcd9cdb9f5d966
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-04-15 20:41:56 +0530

    chewbecca and number

--- conver.java
+++ conver.java
@@ -4,19 +4,29 @@ public class conver {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int lastDigit=0;
-        int newNumber=0;
-        while(n!=0){
-            lastDigit=n%10;
-            if(lastDigit>=5){
-                lastDigit=9-5;
-                newNumber=newNumber*10+lastDigit;
-            }
-            else{
-                newNumber=newNumber*10+lastDigit;
-            }
-            n/=10;
+        int count=0;
+        int N=n;
+        int arr[]=new int[n];
+        if(n==0){
+            System.out.print("0");
         }
-        System.out.println(newNumber);
+        else{
+            while(n!=0){
+                n/=10;
+                count++;
+            }
+            for(int i=count-1;i>=0;i--){
+                arr[i]=N%10;
+                N/=10;
+            }
+            for(int i=0;i<count;i++){
+                if(arr[i]>=5){
+                    arr[i]=9-arr[i];
+                }
+            }
+        }
+        for(int i=0;i<count;i++){
+            System.out.print(arr[i]);
+        }
     }
 }
