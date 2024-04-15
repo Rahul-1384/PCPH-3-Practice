bbbee1cf61f7f83005ff28c708633b54eb9bfc8c
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-04-15 21:27:26 +0530

    s

---
+++ LinkList.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class LinkList {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static void display(Node head){
+        Node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Node head=new Node(-1);
+        Node curr=head;
+        for(int i=0;i<n;i++){
+            Node a=new Node(sc.nextInt());
+            curr.next=a;
+            curr=curr.next;
+        }
+        head=head.next;
+        display(head);
+    }
+}
---
+++ max.java
@@ -0,0 +1,11 @@
+public class max {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,2,4};
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+    }
+}
